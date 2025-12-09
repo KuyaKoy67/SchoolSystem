16eef7a74204d3223576a55b21f5c7b346301aac
Carl Nathan <2540394@edu.vaniercollege.qc.ca>
2025-12-08 22:35:44 -0500

    Add isDepartmentNameValid method and Constructor with only departmentName

--- src/main/java/org/carl/Department.java
+++ src/main/java/org/carl/Department.java
@@ -8,10 +8,34 @@ import lombok.ToString;
 @ToString
 @EqualsAndHashCode
 @Getter
-@Setter
 public class Department {
-    private String departmentId;
-    private String departmentName;
+    private final String departmentId;
+    @Setter private String departmentName;
 
     private static int nextId;
+
+    private static boolean isDepartmentNameValid(String departmentName) {
+        if (departmentName == null || departmentName.isEmpty()) {
+            return false;
+        }
+
+        for (char c : departmentName.toCharArray()) {
+            if (!Character.isLetter(c) && !(c >= '0' && c <= '9')) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public Department(String departmentName) {
+
+        if (isDepartmentNameValid(departmentName)) {
+            this.departmentId = String.format("D%02d", nextId++);
+            this.departmentName = departmentName;
+        } else {
+            this.departmentId = null;
+            this.departmentName = null;
+        }
+    }
 }
